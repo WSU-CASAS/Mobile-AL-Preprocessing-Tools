605c0ba76d86a6be081dbe1de1f845ad3d54aeb7
Bryan Minor <bminor@wsu.edu>
2021-04-05 09:41:34 -0700

    Add events to queue and apply labels. Need to adapt for multiple overlapping label windows.

--- apply_labels.py
+++ apply_labels.py
@@ -5,7 +5,8 @@ preceeding the labels using certain rules.
 import os
 from argparse import ArgumentParser
 from collections import deque
-from datetime import timedelta
+from datetime import timedelta, datetime
+from typing import Optional
 
 from mobiledata import MobileData
 
@@ -40,6 +41,24 @@ class LabelApplier:
 
     When using the filtering, we simply don't write out events that aren't labeled.
     """
+
+    @property
+    def current_window_start(self) -> Optional[datetime]:
+        """The start of the current label window (if any)"""
+
+        if self.current_label_stamp is None:
+            return None
+
+        return self.current_label_stamp - self.window_start
+
+    @property
+    def current_window_end(self) -> Optional[datetime]:
+        """The end of the current label window (if any)"""
+
+        if self.current_label_stamp is None:
+            return None
+
+        return self.current_label_stamp - self.window_end
 
     def __init__(self, in_file: str, out_file: str, window_start_s: float, window_end_s: float,
                  filter_instances: bool = False):
@@ -81,6 +100,10 @@ class LabelApplier:
         self.stamp_field = default_stamp_field
         self.label_field = default_activity_label_field
 
+        # The current label to label with and when it occurs:
+        self.current_label = None  # type: Optional[str]
+        self.current_label_stamp = None  # type: Optional[datetime]
+
     def run_labels(self):
         """Actually run the label application"""
 
@@ -95,10 +118,43 @@ class LabelApplier:
             self.out_data.write_headers()
 
             for in_event in self.in_data.rows_dict:
-                print(in_event)
+                # Add the event to the front of the queue:
+                self.event_queue.append(in_event)
+
+                # Set the current label if the event has one:
+                event_label = in_event[self.label_field]
+
+                if event_label is not None:
+                    self.current_label = event_label
+                    self.current_label_stamp = in_event[self.stamp_field]
+
+                # Process any events from the end of the queue that are now beyond the window_start
+                # time:
+                self.write_events_from_queue(
+                    end_stamp=in_event[self.stamp_field] - self.window_start
+                )
         finally:
             self.in_data.close()
             self.out_data.close()
+
+    def write_events_from_queue(self, end_stamp: Optional[datetime]):
+        """
+        Write files from the back of the queue. Will write all files unless end_stamp is set, at
+        which point it will only write until end_stamp is reached.
+        """
+
+        while len(self.event_queue) > 0 \
+                and (end_stamp is None or self.event_queue[0][self.stamp_field] < end_stamp):
+            # Get the event off the back of the queue:
+            event = self.event_queue.popleft()
+
+            # Apply any existing label to the event if within the current label's window:
+            if self.current_window_start is not None and self.current_window_end is not None:
+                if self.current_window_start <= event[self.stamp_field] <= self.current_window_end:
+                    event[self.label_field] = self.current_label
+
+            # Now write out the label:
+            self.out_data.write_row_dict(event)
 
 
 if __name__ == '__main__':
@@ -132,8 +188,8 @@ if __name__ == '__main__':
             # Add 'instances':
             new_extension = f'.instances{extension}'
         else:
-            # Add 'filtered':
-            new_extension = f'.filtered{extension}'
+            # Add 'labeled':
+            new_extension = f'.labeled{extension}'
 
         output_file = f'{filename}{new_extension}'
 
