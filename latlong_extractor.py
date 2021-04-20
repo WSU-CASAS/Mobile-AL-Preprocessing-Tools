f811b12eba1111bc7354362901cb347260c101e5
Bryan Minor <bminor@wsu.edu>
2021-04-20 14:41:12 -0700

    Implemented lat/long coordinate extraction and writing to file.

--- latlong_extractor.py
+++ latlong_extractor.py
@@ -12,6 +12,27 @@ If the lat/lon are the same as the last seen, it won't write them out since we d
 copies of the same location.
 """
 from argparse import ArgumentParser
+from typing import List, Tuple, Optional
+
+from mobiledata import MobileData
+
+
+def extract_from_files(in_filenames: List[str], out_filename: str):
+    last_coords = None  # type: Optional[Tuple[float, float]]
+
+    with open(out_filename, 'a') as out_file:
+        for in_filename in in_filenames:
+            print(f"Processing {in_filename}")
+
+            with MobileData(in_filename, 'r') as in_data:
+                for event in in_data.rows_dict:
+                    # Only write the event's lat/long if they don't match the last ones written:
+                    if last_coords is None or event['latitude'] != last_coords[0] \
+                            or event['longitude'] != last_coords[1]:
+                        # Write the event to the file:
+                        out_file.write(f"{event['latitude']} {event['longitude']}\n")
+
+                        last_coords = (event['latitude'], event['longitude'])
 
 if __name__ == '__main__':
     parser = ArgumentParser(description="Extract latitude and longitude pairs from CSV data files.")
@@ -25,3 +46,7 @@ if __name__ == '__main__':
     output_file = args.output_file
     if output_file is None:
         output_file = 'latlong'
+
+        print(f"Using default output file name {output_file}")
+
+    extract_from_files(args.input_files, output_file)
